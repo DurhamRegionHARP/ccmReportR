9e4e8278ddfffa6decd36320d7f654535abac24f
James Lane <lanejames35@gmail.com>
2020-10-30 23:12:11 -0400

    First query attempt

--- R/getCases.R
+++ R/getCases.R
@@ -1,2 +1,24 @@
 # Execute a SOQL query against the Case object
 # The Case object maps to Investigations on the client-side
+
+getCases <- function(query, access_token) {
+  # Define some constants
+  resource_uri <- 'https://mohcontacttracing.my.salesforce.com/services/data/v49.0/query/?q='
+  # Validate the query
+  if (is.null(query)) {
+    cat('gotCases: Expected a query to execute...Please try again!\n')
+  } else {
+    # Post the query to Salesforce
+    resp <- GET(
+      url = paste(resource_uri, query, sep=''),
+      add_headers(Authorization = paste('Bearer', access_token))
+    )
+    # Parse the results
+    data <- fromJSON(content(resp, 'text'))
+    if ('MALFORMED_QUERY' %in% names(data)) {
+      cat('Your query returned an error\n')
+    } else {
+      data
+    }
+  }
+}
--- index.R
+++ index.R
@@ -1,5 +1,11 @@
 # Salesforce ReportR
 library(httr)
+library(jsonlite)
 source('./R/auth.R')
+source ('./R/getCases.R')
 
-login()
+access_token <- login()
+getCases(
+  "SELECT+CCM_ReportedDate__c,Id+FROM+Case+WHERE+CCM_New_Diagnosing_PHU__c+=+'a1z5X00000006ZRQAY'+AND+CCM_ReportedDate__c+=+TODAY",
+  access_token
+)
