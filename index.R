7a0821ee3d18e8ea2de00e6728fbab2868db7d57
James Lane <lanejames35@gmail.com>
2020-10-31 14:18:18 -0400

    iterate cases and check their risk factors

--- index.R
+++ index.R
@@ -5,7 +5,20 @@ source('./R/auth.R')
 source ('./R/getCases.R')
 
 access_token <- login()
-getCases(
-  "SELECT+CCM_ReportedDate__c,Id+FROM+Case+WHERE+CCM_New_Diagnosing_PHU__c+=+'a1z5X00000006ZRQAY'+AND+CCM_ReportedDate__c+=+TODAY",
-  access_token
+cases_list <- data.frame(
+  getCases(
+    "SELECT+Epidemiologic_link_Status__c,Id+FROM+Case+WHERE+CCM_New_Diagnosing_PHU__c+=+'a1z5X00000006ZRQAY'+AND+CCM_ReportedDate__c+=+YESTERDAY+AND+CCM_Classification__c+=+'CONFIRMED'+AND+Epidemiologic_linkage__c+=+'Yes'",
+    access_token
+  )$records
 )
+for (row in 1:nrow(cases_list)) {
+  # Look for the required risk factor
+  # based on the value of 'Epidemiologic_link_Status__c'
+  switch (
+    cases_list[row, 'Epidemiologic_link_Status__c'],
+    'Household contact' = checkRiskFactor(cases_list[row, 'Id'], 'Household contact with a case')
+    'Close contact' = checkRiskFactor(cases_list[row, 'Id'], 'Close contact with a case')
+    'Travel' = checkRiskFactor(cases_list[row, 'Id'], 'Travel outside province in the last 14 days (specify province or country)')
+    'Outbreak related' = checkOutbreakRiskFactor(cases_list[row, 'Id'])
+    )
+}
