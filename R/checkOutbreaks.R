619eac2f8e72fddf4be6255071cced0d3a32adcf
James Lane <lanejames35@gmail.com>
2020-11-01 15:57:33 -0500

    function to check outbreak related risk factors

---
+++ R/checkOutbreaks.R
@@ -0,0 +1,8 @@
+# Check and fix outbreak related risk factors
+
+checkOutbreaks <- function(access_token) {
+  # 1. Case => check CCM_Investigation_Outbreak__c
+  # 2. Exposure => check CCM_Exposure_Outbreak__c
+  # 3. Get Exposure_Setting_Type__c
+  # 4. Check for risk factor -- How to determine role (staff vs. resident)
+}
