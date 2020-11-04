0db504a1a7c6c52ee088a99c02039acc75bc1304
James Lane <lanejames35@gmail.com>
2020-11-04 09:57:56 -0500

    Construct the risk factor query

--- R/getRiskFactor.R
+++ R/getRiskFactor.R
@@ -11,4 +11,69 @@ getRiskFactors <- function(options = list()) {
     options$columns <- 'Id'
     options$healthUnit <- NULL
   }
+  # Translate each option to language Salesforce expects
+  statements <- list()
+  if (!is.null(options$to)) {
+    statements$dateRange <- paste(
+      "CCM_ReportedDate__c>='",
+      options$from,
+      "'",
+      " AND ",
+      "CCM_ReportedDate__c<='",
+      options$to,
+      "'",
+      sep='')
+  } else {
+    statements$dateRange <- paste('CCM_ReportedDate__c=', options$from, sep='')
+  }
+  if (!is.null(options$riskFactorType)) {
+    statements$recordTypeId <- paste(
+      "RecordTypeId='",
+      getRecordTypeId(options$riskFactorType),
+      "'",
+      sep=''
+    )
+  }
+  if (!is.null(options$healthUnit)) {
+    statements$phu <- paste(
+      "CCM_New_Diagnosing_PHU__c='",
+      getHealthUnitByName(options$healthUnit),
+      "'",
+      sep=''
+    )
+  }
+  # Build the WHERE clause for the query
+  for (statement in 1:length(statements)) {
+    if (statement == 1) {
+      whereClause <- statements[[statement]]
+    } else {
+      whereClause <- paste(
+        whereClause,
+        "+AND+",
+        statements[[statement]],
+        sep=''
+      )
+    }
+  }
+  query <- paste(
+    "SELECT",
+    paste(options$columns, collapse=","),
+    "FROM+Case",
+    "WHERE",
+    whereClause,
+    sep="+"
+  )
+  # See https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/intro_what_is_rest_api.htm
+  resource_uri <- 'https://mohcontacttracing.my.salesforce.com/services/data/v49.0/query/?q='
+  resp <- GET(
+    url = paste(resource_uri, query, sep=''),
+    add_headers(Authorization = paste('Bearer', key_get('CCM', 'AccessToken')))
+  )
+  warn_for_status(resp, paste('get cases!\n', content(resp)$message))
+  data <- fromJSON(content(resp, 'text'))
+  if ('MALFORMED_QUERY' %in% names(data)) {
+    cat('The query was rejected due to a syntax error.\n')
+  } else {
+    return(data$records)
+  }
 }
