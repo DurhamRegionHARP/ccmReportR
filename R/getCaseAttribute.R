4308c1b5c1ed80bf9d370dafe26d9f33ce214722
James Lane <lanejames35@gmail.com>
2020-11-05 11:39:45 -0500

    Set column names for each attribute query

--- R/getCaseAttribute.R
+++ R/getCaseAttribute.R
@@ -8,23 +8,51 @@ getCaseAttribute <- function(attribute, case) {
   fnOptions <- list(
     riskFactors = list(
       table = 'CCM_Risk_Factor__c',
-      columns = c('Id', 'CCM_Investigation__c', 'RecordTypeId', 'CCM_RiskFactor_Info__c', 'CCM_AdditionalRisk_lnfo__c')
+      columns = c(
+        'Id',
+        'CCM_Investigation__c',
+        'RecordTypeId',
+        'CCM_RiskFactor_Info__c',
+        'CCM_AdditionalRisk_lnfo__c'
+      )
     ),
     symptoms = list(
       table = 'CCM_Risk_Factor__c',
-      columns = c('Id', 'CCM_Investigation__c', 'RecordTypeId', 'CCM_RiskFactor_Info__c', 'CCM_AdditionalRisk_lnfo__c')
+      columns = c(
+        'Id',
+        'CCM_Investigation__c',
+        'RecordTypeId',
+        'CCM_RiskFactor_Info__c',
+        'CCM_AdditionalRisk_lnfo__c'
+      )
     ),
     interventions = list(
-      table = 'CCM_Risk_Factor__c',
-      columns = c('Id', 'CCM_Investigation__c', 'RecordTypeId', 'CCM_RiskFactor_Info__c', 'CCM_AdditionalRisk_lnfo__c')
+      table = 'Hospitalization__c',
+      columns = c(
+        'Id',
+        'Case__c',
+        'RecordTypeId',
+        'Intervention__c',
+        'Intervention_Information__c',
+        'StartDate__c',
+        'EndDate__c',
+        'Hospital__c'
+      )
     ),
     labResults = list(
       table = 'CCM_Risk_Factor__c',
-      columns = c('Id', 'CCM_Investigation__c', 'RecordTypeId', 'CCM_RiskFactor_Info__c', 'CCM_AdditionalRisk_lnfo__c')
+      columns = c(
+        'Id',
+        'Case__c',
+        'CCM_LabOrderID__c',
+        'CCM_ObservationResultStatus__c',
+        'CCM_InterpretationValue__c',
+        'CCM_CollectionDate__c','CCM_ReportedDate__c'
+      )
     ),
   )
   # Map through `cases` and return `attribute` data
   data <- map_dfr(case, function(el) {
-
+    getAttribute(el, fnOptions[[attribute]])
   })
 }
