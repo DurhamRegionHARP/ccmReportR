a049f0372a91b99c4938479d025c10e01232a500
James Lane <lanejames35@gmail.com>
2020-11-11 09:15:42 -0500

    getHealthUnitByName: initial documentation

--- R/getHealthUnitByName.R
+++ R/getHealthUnitByName.R
@@ -1,4 +1,12 @@
-# Look up a Health Unit's Id by name
+#' Look up a Health Unit's Id by name
+#'
+#' `getHealthUnitByName()` searches the PHU_Stage_Table__c object
+#' and returns the Id for the specified health unit.
+#'
+#' @param healthUnitName character scalar or vector. Names the
+#'   health unit to use when searching for an Id.
+#' @returns If the query succeeds, a character scalar or vector
+#'   of health unit Id's. Otherwise an error is returned.
 
 getHealthUnitByName <- function(healthUnitName) {
   resp <- GET(
@@ -13,5 +21,10 @@ getHealthUnitByName <- function(healthUnitName) {
   )
   stop_for_status(resp, "get a health unit ID")
   data <- fromJSON(content(resp, 'text'))
+  if (!data$totalSize) {
+    stop(
+      paste('There is no health unit with name', healthUnitName, '\n', sep = '')
+    )
+  }
   return(data$records$Id)
 }
